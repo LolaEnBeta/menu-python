c83c6ca6b3295a0fc0bb561a6d83f6db8fce05a3
LolaEnBeta <lolitaenbeta@gmail.com>
2019-04-12 17:13:55 +0200

    Added a menu for weekend

--- menu-program.py
+++ menu-program.py
@@ -1,14 +1,23 @@
 
 import random
 
-week = ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY"]
+week = ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY"]
+weekend = ["SATURDAY", "SUNDAY"]
 
 with open("menu-list.txt", "r") as menus:
     meals = []
     for meal in menus:
         meals.append(meal.split())
 
+with open("weekend_menu.txt", "r") as weekend_menus:
+    weekend_meals = []
+    for meal in weekend_menus:
+        weekend_meals.append(meal.split())
+
 for day in week:
     random.shuffle(meals)
     print(day, "\n", meals.pop())
     
+for day in weekend:
+    random.shuffle(weekend_meals)
+    print(day, "\n", "Lunch", "\n", weekend_meals.pop(), "\n", "Dinner", "\n", weekend_meals.pop())
